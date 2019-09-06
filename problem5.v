80b50c238f9e8051b78a655c6bc3a333e7893800
Bryan Orabutt <borabutt@wustl.edu>
2019-09-06 10:02:14 -0500

    Working on FSM problem

--- problem5.v
+++ problem5.v
@@ -2,8 +2,14 @@
 module problem5(rst, req_0, req_1, gnt_0, gnt_1, idle);
 
 input rst, req_0, req_1;
-output reg gnt_0, gnt_1, idle;
+output gnt_0, gnt_1, idle;
+output reg[2:0] cur_state, next_state;
 
+localparam IDLE=3'b001, GNT0=3'b010, GNT1=3'b100;
+
+always@(rst) begin
+	if(rst) state <= IDLE;
+end
 
 
 endmodule
